5141b12522ae69b15170681ac0f8f9e1ab13390e
gabrielSilvestre444 <gabrielmax.silvestre@gmail.com>
2026-07-23 21:24:06 -0600

    Números divisibles de 3 y 5

--- Numeros_divisibles_3_y_5.py
+++ Numeros_divisibles_3_y_5.py
@@ -2,6 +2,4 @@ print("Números divisibles por 3 y 5 (1-100): ")
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print(i, end=" ")
-print() # salto de línea final 
-
-print("Holaaa")
+print() # salto de línea final
